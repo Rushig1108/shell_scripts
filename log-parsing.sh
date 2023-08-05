5aa799130060943d295e9fb0a6091cdbaf07fb91
Ubuntu <ubuntu@ip-172-31-80-254.ec2.internal>
2023-08-05 13:15:35 +0000

    Added a file that sorts out errors from a log file

---
+++ log-parsing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+log_file="$1"
+output_file="error.txt"
+
+# Search for error lines and extract timestamps
+grep -i "error" "$log_file" | awk -F'[][]' '{print "Error at", $2 ":", $NF}' > "$output_file"
+
+echo "Error lines with timestamps have been saved to: $output_file"
+
